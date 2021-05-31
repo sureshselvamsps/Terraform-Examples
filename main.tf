a2e489a270b1beaf85946f01912bb4f3a32acb41
sureshselvamsps <63282381+sureshselvamsps@users.noreply.github.com>
2021-05-31 18:21:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 resource "aws_instance" "myinstance" { 
-    ami = "ami-0d5eff06f840b45e9 "
+    ami = "ami-0d5eff06f840b45e9"
     instance_type = "t2.micro"
     tags = {
     Name = "Terraform Cloud"
