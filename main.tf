198a2903a4e1ae731b926d5ecc0db4a668147f11
sureshselvamsps <63282381+sureshselvamsps@users.noreply.github.com>
2021-06-03 18:59:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ resource "aws_instance" "myinstance" {
     ami = "ami-0d5eff06f840b45e9"
     instance_type = "t2.micro"
     tags = {
-    Name = "Terraform Cloud"
+    Name = "Terraform Cloud testing"
   }
 }
