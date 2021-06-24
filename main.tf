d19fce07a9f4e15de8894af34123e27c955859b0
sureshselvamsps <63282381+sureshselvamsps@users.noreply.github.com>
2021-06-24 11:41:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,35 @@
+resource "vault_aws_secret_backend" "aws" {
+  access_key = "AKIAXBTJUNSWPOQMOQ4X"
+  secret_key = "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"
+}
+
+resource "vault_aws_secret_backend_role" "role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "test"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "iam:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
+}
+
+# generally, these blocks would be in a different module
+data "vault_aws_access_credentials" "creds" {
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.role.name
+}
+
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
   region = var.aws_region
 }
 resource "aws_instance" "insta" { 
