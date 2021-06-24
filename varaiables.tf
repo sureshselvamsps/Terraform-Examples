459299710fe278967483b15fdd9fa13740d48696
sureshselvamsps <63282381+sureshselvamsps@users.noreply.github.com>
2021-06-24 11:27:03 +0530

    Update varaiables.tf

--- varaiables.tf
+++ varaiables.tf
@@ -2,3 +2,11 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+variable "aws_access_key" {
+  type    = string
+  default = "AKIAXBTJUNSWPOQMOQ4X"
+}
+variable "aws_secret_key" {
+  type    = string
+  default = "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"
+}
